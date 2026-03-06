e0ec41bc253d99c64c738e0ab97e3c1e75140640
alexperezortuno <alexperezortuno@gmail.com>
2026-03-05 22:27:54 -0300

    add hash_password utility for generating bcrypt hashes

---
+++ hash_password.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Uso: go run hash_password.go <password>")
+		return
+	}
+	password := os.Args[1]
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println(string(hash))
+}
