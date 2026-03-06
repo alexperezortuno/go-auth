09940090a558138dc6a5c469784f4b026e6a10b2
alexperezortuno <alexperezortuno@gmail.com>
2026-03-05 22:28:17 -0300

    refactor Response struct to use UUID for ID field

--- internal/platform/storage/user/user_storage.go
+++ internal/platform/storage/user/user_storage.go
@@ -1,11 +1,13 @@
 package user
 
+import "github.com/google/uuid"
+
 type Response struct {
-	ID       uint32 `json:"id" copier:"must"`
-	FullName string `json:"full_name"`
-	Name     string `json:"name"`
-	LastName string `json:"last_name"`
-	Nickname string `json:"nickname"`
-	IdCard   string `json:"id_card"`
-	Email    string `json:"email" copier:"must"`
+	ID       uuid.UUID `json:"id" copier:"must"`
+	FullName string    `json:"full_name"`
+	Name     string    `json:"name"`
+	LastName string    `json:"last_name"`
+	Nickname string    `json:"nickname"`
+	IdCard   string    `json:"id_card"`
+	Email    string    `json:"email" copier:"must"`
 }
